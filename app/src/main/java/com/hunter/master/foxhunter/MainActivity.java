b21eaae90a3d83d5aaf999ce87a4c09e9a3e83c5
weiyan <994334509@qq.com>
2016-10-10 10:25:14 +0800

    create test activities

--- app/src/main/java/com/hunter/master/foxhunter/MainActivity.java
+++ app/src/main/java/com/hunter/master/foxhunter/MainActivity.java
@@ -1,13 +1,31 @@
 package com.hunter.master.foxhunter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
+    String tests[] = {"Communication","GameLogic","UserInterface"};
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setListAdapter(new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,tests));
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onListItemClick(ListView list, View view, int position, long id){
+        super.onListItemClick(list, view, position, id);
+        String testName = tests[position];
+        try{
+            @SuppressWarnings("rawtypes")
+            Class clazz = Class.forName("com.hunter.master.foxhunter"+testName);
+            Intent intent = new Intent(this,clazz);
+            startActivity(intent);
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ app/src/main/java/com/hunter/master/foxhunter/UserInterface.java
@@ -0,0 +1,13 @@
+package com.hunter.master.foxhunter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_interface);
+    }
+}
