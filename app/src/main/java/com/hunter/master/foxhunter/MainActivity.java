460354cf7dfbc8525117a7956fde554c1d3ddae7
abgnwl <abgnwl@163.com>
2016-10-30 17:14:09 +0800

    try network connect

--- app/src/main/java/com/hunter/master/foxhunter/MainActivity.java
+++ app/src/main/java/com/hunter/master/foxhunter/MainActivity.java
@@ -3,13 +3,16 @@ package com.hunter.master.foxhunter;
 import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import java.lang.String;
 
 public class MainActivity extends ListActivity {
     String tests[] = {"Communication","GameLogic","UserInterface"};
     public void onCreate(Bundle savedInstanceState){
+        Log.d("lzj","mainstart");
         super.onCreate(savedInstanceState);
         setListAdapter(new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,tests));
@@ -20,11 +23,13 @@ public class MainActivity extends ListActivity {
         super.onListItemClick(list, view, position, id);
         String testName = tests[position];
         try{
+            Log.d("lzj",testName);
             @SuppressWarnings("rawtypes")
-            Class clazz = Class.forName("com.hunter.master.foxhunter"+testName);
+            Class clazz = Class.forName("com.hunter.master.foxhunter."+testName);
             Intent intent = new Intent(this,clazz);
             startActivity(intent);
         }catch(ClassNotFoundException e){
+            Log.d("lzj","ERROR");
             e.printStackTrace();
         }
     }
