482050d75d4b20fea69bafbc2067c404c6b30bfa
BG2CYR <846849587@qq,com>
2016-11-12 01:30:49 +0800

    add loc libs

--- app/src/main/java/com/hunter/master/foxhunter/GameLogic.java
+++ app/src/main/java/com/hunter/master/foxhunter/GameLogic.java
@@ -3,8 +3,10 @@ package com.hunter.master.foxhunter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GameLogic extends AppCompatActivity {
+import com.gps.LOC_IF;
 
+public class GameLogic extends AppCompatActivity {
+    LOC_IF loc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
