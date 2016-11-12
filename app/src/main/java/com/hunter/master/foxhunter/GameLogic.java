23fa84af0e01fdcd3e2d539f9d12f6e7d9f8279e
BG2CYR <846849587@qq,com>
2016-11-12 23:58:41 +0800

    use loc interface

--- app/src/main/java/com/hunter/master/foxhunter/GameLogic.java
+++ app/src/main/java/com/hunter/master/foxhunter/GameLogic.java
@@ -2,14 +2,20 @@ package com.hunter.master.foxhunter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.gps.LOC_IF;
 
 public class GameLogic extends AppCompatActivity {
     LOC_IF loc;
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_logic);
+        text= (TextView) findViewById(R.id.loctext);
+        loc=new LOC_IF(getApplicationContext());
+        loc.setShowID(text);
+        loc.startLocation();
     }
 }
